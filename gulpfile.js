9d1e90c9f59c7face20984122a7f420efdf6d545
vbudhram <vbudhram@gmail.com>
2014-09-02 20:11:06 -0400

    Updated gulp script to create views folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,69 @@
-var gulp    = require('gulp'),
-  less      = require('gulp-less'),
-  usemin    = require('gulp-usemin'),
-  wrap      = require('gulp-wrap');
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    usemin = require('gulp-usemin'),
+    wrap = require('gulp-wrap');
 
-var paths = {
-  js: 'web/js/**/*.*',
-  fonts: 'web/fonts/**.*',
-  images: 'web/img/**/*.*',
-  styles: 'web/less/**/*.less',
-  index: 'web/index.html',
-  bower_fonts: 'web/bower_components/**/*.{ttf,woff,eof,svg}',
-  bower_components: 'web/bower_components/**/*.*',
-  watch_path: 'web/less/dashboard/**/*.less'
+var paths = {
+    js: 'web/js/**/*.*',
+    fonts: 'web/fonts/**.*',
+    images: 'web/img/**/*.*',
+    styles: 'web/less/**/*.less',
+    index: 'web/index.html',
+    views: 'web/views/**/*.*',
+    bower_fonts: 'web/bower_components/**/*.{ttf,woff,eof,svg}',
+    bower_components: 'web/bower_components/**/*.*',
+    watch_path: 'web/less/dashboard/**/*.less'
 };
 
 
-gulp.task('usemin', function() {
-  return gulp.src(paths.index)
-    .pipe(usemin({
-      less: [less(), 'concat'],
-      js: ['concat', wrap('(function(){ \n<%= contents %>\n})();')],
-    }))
-    .pipe(gulp.dest('dist/'));
+gulp.task('usemin', function () {
+    return gulp.src(paths.index)
+        .pipe(usemin({
+            less: [less(), 'concat'],
+            js: ['concat', wrap('(function(){ \n<%= contents %>\n})();')],
+        }))
+        .pipe(gulp.dest('dist/'));
 });
 
 /**
  * Copy assets
  */
-gulp.task('copy-assets', ['copy-images', 'copy-fonts', 'copy-bower_fonts']);
+gulp.task('copy-assets', ['copy-images', 'copy-fonts', 'copy-bower_fonts', 'copy-views']);
 
-gulp.task('copy-images', function(){
-  return gulp.src(paths.images)
-    .pipe(gulp.dest('dist/img'));
+gulp.task('copy-images', function () {
+    return gulp.src(paths.images)
+        .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('copy-fonts', function(){
-  return gulp.src(paths.fonts)
-    .pipe(gulp.dest('dist/fonts'));
+gulp.task('copy-fonts', function () {
+    return gulp.src(paths.fonts)
+        .pipe(gulp.dest('dist/fonts'));
 });
 
-gulp.task('copy-bower_fonts', function(){
-  return gulp.src(paths.bower_fonts)
-    .pipe(gulp.dest('dist/lib'));
+gulp.task('copy-bower_fonts', function () {
+    return gulp.src(paths.bower_fonts)
+        .pipe(gulp.dest('dist/lib'));
+});
+
+gulp.task('copy-views', function () {
+    return gulp.src(paths.views)
+        .pipe(gulp.dest('dist/views'));
 });
 
 /**
  * Compile less
  */
-gulp.task('compile-less', function(){
-  return gulp.src(paths.styles)
-      .pipe(less())
-      .pipe(gulp.dest('dist/css'));
+gulp.task('compile-less', function () {
+    return gulp.src(paths.styles)
+        .pipe(less())
+        .pipe(gulp.dest('dist/css'));
 });
 
 /**
  * Watch less
  */
-gulp.task('watch-less', function() {
-  gulp.watch(paths.watch_path, ['compile-less']);
+gulp.task('watch-less', function () {
+    gulp.watch(paths.watch_path, ['compile-less']);
 });
 
 gulp.task('build', ['usemin', 'copy-assets']);
